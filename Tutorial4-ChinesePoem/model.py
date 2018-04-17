8ee85b428ecff8df6c16bf8ec1364538716fef50
Will Feng <willfeng@fb.com>
2018-04-17 00:05:09 -0400

    clean up model

--- Tutorial4-ChinesePoem/model.py
+++ Tutorial4-ChinesePoem/model.py
@@ -1,7 +1,6 @@
 # coding:utf8
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
 import torch.nn.functional as F
 
 
@@ -9,18 +8,15 @@ class PoetryModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim):
         super(PoetryModel, self).__init__()
         self.hidden_dim = hidden_dim
-        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, self.hidden_dim, num_layers=2)
-        self.linear1 = nn.Linear(self.hidden_dim, vocab_size)
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim).cuda()
+        self.lstm = nn.LSTM(embedding_dim, self.hidden_dim, num_layers=2).cuda()
+        self.linear1 = nn.Linear(self.hidden_dim, vocab_size).cuda()
 
     def forward(self, input, hidden=None):
         seq_len, batch_size = input.size()
         if hidden is None:
-            #  h_0 = 0.01*torch.Tensor(2, batch_size, self.hidden_dim).normal_().cuda()
-            #  c_0 = 0.01*torch.Tensor(2, batch_size, self.hidden_dim).normal_().cuda()
-            h_0 = input.data.new(2, batch_size, self.hidden_dim).fill_(0).float()
-            c_0 = input.data.new(2, batch_size, self.hidden_dim).fill_(0).float()
-            h_0, c_0 = Variable(h_0), Variable(c_0)
+            h_0 = input.data.new(2, batch_size, self.hidden_dim).fill_(0).float().cuda()
+            c_0 = input.data.new(2, batch_size, self.hidden_dim).fill_(0).float().cuda()
         else:
             h_0, c_0 = hidden
         # size: (seq_len,batch_size,embeding_dim)
