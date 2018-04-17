158e9838ed84f56d4caa1951c682f1961d689876
Will Feng <willfeng@fb.com>
2018-04-17 00:06:27 -0400

    clean up model

--- Tutorial4-ChinesePoem/model.py
+++ Tutorial4-ChinesePoem/model.py
@@ -8,15 +8,15 @@ class PoetryModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim):
         super(PoetryModel, self).__init__()
         self.hidden_dim = hidden_dim
-        self.embeddings = nn.Embedding(vocab_size, embedding_dim).cuda()
-        self.lstm = nn.LSTM(embedding_dim, self.hidden_dim, num_layers=2).cuda()
-        self.linear1 = nn.Linear(self.hidden_dim, vocab_size).cuda()
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, self.hidden_dim, num_layers=2)
+        self.linear1 = nn.Linear(self.hidden_dim, vocab_size)
 
     def forward(self, input, hidden=None):
         seq_len, batch_size = input.size()
         if hidden is None:
-            h_0 = input.data.new(2, batch_size, self.hidden_dim).fill_(0).float().cuda()
-            c_0 = input.data.new(2, batch_size, self.hidden_dim).fill_(0).float().cuda()
+            h_0 = input.data.new(2, batch_size, self.hidden_dim).fill_(0).float()
+            c_0 = input.data.new(2, batch_size, self.hidden_dim).fill_(0).float()
         else:
             h_0, c_0 = hidden
         # size: (seq_len,batch_size,embeding_dim)
